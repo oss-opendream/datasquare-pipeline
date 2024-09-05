65a1674d54a65e045e5d55eb0a00286ce77f520f
wooyeol.park <wooyeol.dev@gmail.com>
2024-09-05 11:18:11 +0000

    fix: Changed Docker-compose files in kafka cluster run and down script

--- shell/kafka_down_all.sh
+++ shell/kafka_down_all.sh
@@ -3,11 +3,11 @@
 servers=("server1" "server2" "server3")
 
 # 서버에 맞는 compose 파일 배열
-compose_files=("docker-compose-server1.yml" "docker-compose-server2.yml" "docker-compose-server3.yml")
+compose_files=("docker-compose-kafka1.yml" "docker-compose-kafka2.yml" "docker-compose-kafka3.yml")
 
 # 반복문을 사용하여 서버별로 docker-compose 명령 실행
 for i in ${!servers[@]}; do
-  echo "Docker composing server{$i} down..."
+  echo "Docker composing server[$i] down..."
   DOCKER_CMD="docker compose -f ~/datasquare-pipeline/docker/yml/${compose_files[$i]} down"
-  ssh ${servers[$i]} "$DOCKER_CMD"
+  ssh $servers[$i] "$DOCKER_CMD"
 done
