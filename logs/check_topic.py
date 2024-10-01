0756d0eb5bdfb538cd481f6713f440b4e0c3e1ba
yeonhlee <lyhldy@naver.com>
2024-10-01 09:00:37 +0000

    refact: bootstrap.servers, topic_name renamiaming

--- logs/check_topic.py
+++ logs/check_topic.py
@@ -4,11 +4,11 @@ import time
 from confluent_kafka.admin import AdminClient, NewTopic
 
 if __name__ == '__main__':
-    conf = {'bootstrap.servers': 'server1:9092, server2:9092, server3:9092'}
+    conf = {'bootstrap.servers': 'kafka1:9092, kafka2:9092, kafka39092'}
     kadmin = AdminClient(conf)
     topic_dict = kadmin.list_topics().topics
 
-    topic_name = 'datasquare_web_log2'
+    topic_name = 'datasquare_web_log'
 
     # while len(topic_dict) <= 1:
     #     print('Bootstrap servers are not ready yet. \nRetry after 5 seconds')
@@ -18,7 +18,6 @@ if __name__ == '__main__':
 
     if topic_name in topic_dict:
         print(f"'{topic_name}' topic found")
-
     else:
         print(f"'{topic_name}' topic not found. \nNew topic '{topic_name}' will be created.")
         time.sleep(2)
